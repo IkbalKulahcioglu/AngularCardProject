3362415640c8580811ba2caa35329f74bb0a055e
IkbalKulahcioglu <ikbalkulahcioglu@gmail.com>
2022-12-02 00:42:56 +0300

    component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,25 @@ export class AppComponent {
   constructor() {
     console.log(environment.production); // Logs false for default environment
   }
-  title = 'card';
+  
+posts=[
+  {
+    title:'Biking',
+    imageURL:'assets/biking.jpeg',
+    username:'ikbiking',
+    content:'It was a nice bike tour'
+  },
+  {
+    title:'Mountain Climbing',
+    imageURL:'assets/mountain.jpeg',
+    username:'ikMount',
+    content:'My mountain climbing experience today'
+  },
+  {
+    title:'Hiking',
+    imageURL:'assets/tree.jpeg',
+    username:'iknature',
+    content:'Let s take a walk in nature'
+  },
+]
 }
